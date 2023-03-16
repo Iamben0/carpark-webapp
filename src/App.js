db7c0ccc86216c94b8fa88d10855377ead9e2446
iamben0 <benedict.ongjp@gmail.com>
2023-03-17 02:03:41 +0800

    added CarparAavailability.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ function App() {
   const timestamp = '2023-03-16T20:18:11'; // replace with your desired timestamp
 
   return (
+    // let timestamp = (
+    //   <Input
+    //     placeholder='Select Date and Time'
+    //     size='md'
+    //     type='datetime-local'
+    //   />
+    // );
     <>
       <CarparkAvailability timestamp={timestamp} />
       {/* <CarparkAvailability /> */}
