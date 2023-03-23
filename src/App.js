849f6d4f97f46c8882105aea0a1e480f2ded9b26
iamben0 <benedict.ongjp@gmail.com>
2023-03-23 15:21:21 +0800

    added date component to search via date and time

--- src/App.js
+++ src/App.js
@@ -2,19 +2,9 @@ import React from 'react';
 import CarparkAvailability from './CarparkAvailability';
 
 function App() {
-  const timestamp = '2023-03-16T20:18:11'; // replace with your desired timestamp
-
   return (
-    // let timestamp = (
-    //   <Input
-    //     placeholder='Select Date and Time'
-    //     size='md'
-    //     type='datetime-local'
-    //   />
-    // );
     <>
-      <CarparkAvailability timestamp={timestamp} />
-      {/* <CarparkAvailability /> */}
+      <CarparkAvailability />
     </>
   );
 }
--- src/CarparkAvailability.js
+++ src/CarparkAvailability.js
@@ -1,8 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  Box,
+  Flex,
+  Input,
+} from '@chakra-ui/react';
 import axios from 'axios';
 
-function CarparkAvailability({ timestamp }) {
+function CarparkAvailability() {
+  const [timestamp, setTimestamp] = useState(new Date().toISOString());
   const [carparkData, setCarparkData] = useState([]);
+  const readableTimestamp = new Date(timestamp).toLocaleString();
+  
   useEffect(() => {
     const encodedTimestamp = encodeURIComponent(timestamp); // URL encode timestamp
     const apiUrl = `https://api.data.gov.sg/v1/transport/carpark-availability?date_time=${encodedTimestamp}`;
@@ -17,30 +32,69 @@ function CarparkAvailability({ timestamp }) {
       });
   }, [timestamp]);
 
+  const handleTimestampChange = (event) => {
+    setTimestamp(event.target.value);
+  };
+
   return (
-    <div>
-      <h1>Carpark Availability ({timestamp})</h1>
-      <table border='1'>
-        <thead>
-          <tr>
-            <th>Carpark</th>
-            <th>Lot Type</th>
-            <th>Total Lots</th>
-            <th>Lots Available</th>
-          </tr>
-        </thead>
-        <tbody>
-          {carparkData.map((carpark, index) => (
-            <tr key={index}>
-              <td>{carpark.carpark_number}</td>
-              <td>{carpark.carpark_info[0].lot_type}</td>
-              <td>{carpark.carpark_info[0].total_lots}</td>
-              <td>{carpark.carpark_info[0].lots_available}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <Flex
+      justifyContent='center'
+      flexDirection='column'
+      alignItems='center'>
+      <Flex
+        justifyContent='center'
+        fontSize='lg'
+        pt='5'>
+        Carpark Availability ({readableTimestamp})
+      </Flex>
+      <Input
+        name='timestamp'
+        placeholder='Select Date and Time'
+        size='md'
+        type='datetime-local'
+        value={timestamp}
+        onChange={handleTimestampChange}
+        maxWidth='300'
+        mb='5'
+        mt='5'
+      />
+      <Box
+        overflowY='auto'
+        maxHeight='300px'
+        maxWidth='500px'>
+        <Table colorScheme='telegram'>
+          <Thead
+            position='sticky'
+            top='0'
+            background='teal'>
+            <Tr>
+              <Th>Carpark</Th>
+              <Th>Lot Type</Th>
+              <Th>Total Lots</Th>
+              <Th>Lots Available</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {carparkData.map((carpark, index) => (
+              <Tr key={index}>
+                <Td>{carpark.carpark_number}</Td>
+                <Td>{carpark.carpark_info[0].lot_type}</Td>
+                <Td>{carpark.carpark_info[0].total_lots}</Td>
+                <Td>{carpark.carpark_info[0].lots_available}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+          <Tfoot>
+            <Tr>
+              <Th>Carpark</Th>
+              <Th>Lot Type</Th>
+              <Th>Total Lots</Th>
+              <Th>Lots Available</Th>
+            </Tr>
+          </Tfoot>
+        </Table>
+      </Box>
+    </Flex>
   );
 }
 
