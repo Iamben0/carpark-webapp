7ebc86e89b83055ef59d59b82a4fea163105c068
iamben0 <benedict.ongjp@gmail.com>
2023-10-04 10:25:03 +0800

    change port number

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors'); // allows cross origin resource sharing
 require('dotenv').config({ path: './config.env' }); // loads environment variables from a .env file into process.env file, this lets you separate configuration files from the code
-const port = process.env.PORT || 5000; // const port process.env.port will access the port variable from the config.env we required
+const port = process.env.PORT || 8080; // const port process.env.port will access the port variable from the config.env we required
 app.use(cors());
 app.use(express.json());
 app.use(require('./routes/record'));
